72262c93feacb330bb7d25dd4217f0cf2ef9a2e5
Jeronimo Aguirre Estrada <jeronimo.aguirre@globant.com>
2025-02-18 13:53:53 -0500

    first commit

---
+++ resources/store.py
@@ -0,0 +1,47 @@
+from flask.views import MethodView
+from flask_smorest import Blueprint, abort
+from sqlalchemy.exc import SQLAlchemyError, IntegrityError
+
+from models import StoreModel
+
+from db import db
+
+from schemas import StoreSchema, ItemSchema
+
+
+blp = Blueprint("stores",__name__, description="Operations on stores")
+
+
+@blp.route("/store/<string:store_id>")
+class Store(MethodView):
+    @blp.response(200, StoreSchema)
+    def get(self, store_id):
+        store = db.query.get_or_404(store_id)
+        return store
+
+    def delete(self, store_id):
+        raise NotImplementedError("Deleting a store is not implemented.")
+
+
+@blp.route("/store")
+class StoreList(MethodView):
+    @blp.response(200, ItemSchema(many=True))
+    def get(self):
+        return StoreModel.query.all()
+
+    @blp.arguments(StoreSchema)
+    @blp.response(201, StoreSchema)
+    def post(self, store_data):
+        store = StoreModel(**store_data)
+        try:
+            db.session.add(store)
+            db.session.commit()
+        except IntegrityError:
+            abort(
+                400,
+                message="A store with that name already exists"
+            )
+        except SQLAlchemyError:
+            abort(500, message="An error has ocurred while inserting the item")
+        
+        return store
